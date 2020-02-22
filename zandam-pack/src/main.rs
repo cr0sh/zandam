7e1befa0e4138daae9aee78ea10b719f524bce4c
Nam Jeonghyun <ska827@snu.ac.kr>
2020-02-23 04:13:21 +0900

    zandam-pack 0.1.0 publish

---
+++ zandam-pack/src/main.rs
@@ -0,0 +1,93 @@
+use encoding_rs::EUC_KR;
+use maplit::hashmap;
+use rpassword::prompt_password_stdout;
+use std::error::Error;
+use std::io::{stdin, stdout, Read, Write};
+use tempfile::tempdir;
+use text_template::Template;
+
+const MAPLESTORY_REG_KEY: &str = r#"HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Wizet\MapleStory"#;
+const ZANDAM_WASM: &[u8] = include_bytes!("../../zandam/pkg/zandam_bg.wasm");
+const ZANDAM_JS: &str = include_str!("../../zandam/pkg/zandam.js");
+const HTML_TEMPLATE: &str = include_str!("../template.html");
+
+fn decode_maybe_euckr(x: Vec<u8>) -> String {
+    match String::from_utf8(x.clone()) {
+        Ok(s) => s,
+        Err(_) => {
+            let (cow, _enc_used, had_errors) = EUC_KR.decode(&x);
+            if had_errors {
+                panic!("해당 문자열을 UTF-8 또는 EUC-KR로 해석할 수 없습니다. (손상된 데이터일 수 있음)");
+            }
+            cow.to_string()
+        }
+    }
+}
+
+fn main() {
+    match std::panic::catch_unwind(main_) {
+        Ok(Ok(())) => (),
+        Ok(Err(e)) => {
+            println!("오류 발생: {}", e);
+            println!("<엔터>를 누르면 프로그램이 종료됩니다.");
+            stdout().flush().unwrap();
+            stdin().lock().read(&mut [0]).unwrap();
+        }
+        Err(_) => {
+            println!(
+                "복구 불가능한 오류가 발생하였습니다. 개발자에게 위 내용을 복사해 전달해 주세요."
+            );
+            println!("<엔터>를 누르면 프로그램이 종료됩니다.");
+            stdout().flush().unwrap();
+            stdin().read(&mut [0]).unwrap();
+        }
+    }
+}
+
+fn main_() -> Result<(), Box<dyn Error>> {
+    let tmpdir = tempdir()?;
+    let file_path = tmpdir.path().join("maple.reg");
+
+    let reg_output = std::process::Command::new("reg")
+        .args(&["export", MAPLESTORY_REG_KEY, file_path.to_str().unwrap()])
+        .output()
+        .expect("Failed to execute process");
+
+    if !reg_output.status.success() {
+        println!("레지스트리 추출이 실패하였습니다. `reg` 명령어 출력:");
+        println!("{}", decode_maybe_euckr(reg_output.stderr));
+        return Err("레지스트리 추출 실패")?;
+    }
+
+    let registry = decode_maybe_euckr(
+        std::fs::read(file_path).map_err(|e| format!(".reg 파일 읽기 실패: {}", e))?,
+    );
+
+    let pass1 =
+        prompt_password_stdout("비밀번호 입력: ").expect("패스워드를 콘솔에서 읽어올 수 없음");
+    let pass2 =
+        prompt_password_stdout("비밀번호 다시 입력: ").expect("패스워드를 콘솔에서 읽어올 수 없음");
+
+    if pass1 != pass2 {
+        return Err("두 비밀번호가 다릅니다.")?;
+    }
+
+    let encrypted = zandam::encrypt(&registry, &pass1);
+    let template = Template::from(HTML_TEMPLATE);
+
+    let wasm_base64 = base64::encode(ZANDAM_WASM);
+    let enc_base64 = base64::encode(&encrypted);
+    let args = hashmap! {
+        "wasm" => wasm_base64.as_ref(),
+        "encrypted" => enc_base64.as_ref(),
+        "js" => ZANDAM_JS,
+        "z" => "${z}",
+        "e.src" => "${e.src}",
+    };
+    let out_html = template.fill_in(&args).to_string();
+
+    std::fs::write("zandam.html", out_html)?;
+
+    tmpdir.close()?;
+    Ok(())
+}
--- zandam/src/lib.rs
+++ zandam/src/lib.rs
@@ -1,7 +1,81 @@
+use aes_soft::Aes256;
+use block_modes::block_padding::Pkcs7;
+use block_modes::{BlockMode, Cbc};
+use hex_literal::hex;
+use sha2::{Digest, Sha256};
+use std::panic;
+use wasm_bindgen::prelude::*;
+
+const SHA_SALT: &str = "vlkh3EOIfr";
+const AES_IV: [u8; 16] = hex!("abcddeadbeefbcdaabcddeadbeefbcda");
+
+type Aes256Cbc = Cbc<Aes256, Pkcs7>;
+
+#[wasm_bindgen(start)]
+pub fn setup_hook() {
+    panic::set_hook(Box::new(console_error_panic_hook::hook));
+}
+
+/// 지정된 문자열에 솔트를 적절히 추가한 후 해싱한 결과를 반환합니다.
+pub(crate) fn generate_key(text: &str) -> Vec<u8> {
+    let mut hasher = Sha256::new();
+    hasher.input(SHA_SALT);
+    hasher.input(text);
+    hasher.input(SHA_SALT);
+
+    hasher.result().to_vec()
+}
+
+/// 지정된 입력 문자열을 AES-256-CBC 방식으로 암호화합니다.
+#[wasm_bindgen]
+pub fn encrypt(input: &str, passwd: &str) -> Vec<u8> {
+    let key = generate_key(passwd);
+    let cipher = Aes256Cbc::new_var(&key, &AES_IV).unwrap();
+    cipher.encrypt_vec(input.as_bytes())
+}
+
+/// 지정된 암호문을 복호화합니다.
+#[wasm_bindgen]
+pub fn decrypt(input: &[u8], passwd: &str) -> Vec<u8> {
+    let key = generate_key(passwd);
+    let cipher = Aes256Cbc::new_var(&key, &AES_IV).unwrap();
+
+    cipher.decrypt_vec(input).unwrap()
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn keygen() {
+        assert_eq!(
+            &generate_key("hello")[..],
+            &hex!("d70ca475de18784adef51c8e6106c2fd737a436b32ab78f9625ec02c9a8599b2")[..]
+        );
+    }
+
+    #[test]
+    fn key_consistency() {
+        let key = generate_key("foo");
+        for _ in 0..100 {
+            let key_ = generate_key("foo");
+            assert_eq!(key, key_);
+        }
+    }
+
+    #[test]
+    fn encdec_identity() {
+        for (input, pass) in &[
+            ("hello", "pass"),
+            ("world", "pass1"),
+            ("my", "bla"),
+            ("name", "blah"),
+        ] {
+            assert_eq!(
+                decrypt(&encrypt(input, pass), pass).as_slice(),
+                input.as_bytes()
+            );
+        }
     }
 }
